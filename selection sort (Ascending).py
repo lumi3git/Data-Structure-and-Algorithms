acd13f5372672e29edc49abc7f7f04deb15b8c52
Dumindu Shanilka <151896892+lumi3git@users.noreply.github.com>
2024-06-24 19:27:28 +0530

    Add files via upload

---
+++ selection sort (Ascending).py
@@ -0,0 +1,25 @@
+def selectionSort(array,size):
+    for step in range(size):
+        min_idx = step
+
+        for i in range(step + 1, size):
+
+            if array[i] < array[min_idx]:
+                min_idx = i
+
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+
+data = [50,85,23,-41,25]
+
+unsorted_data = data.copy()
+
+size = len(data)
+
+selectionSort(data ,size)
+
+print("Unsorted Array is:")
+print(unsorted_data)
+
+print("\nSorted Array is:")
+print(data)
